b79c6eba90a7f02973b8d9cc9c99cce9b9af59da
Jerry <jerrylalrinmawia@gmail.com>
2025-04-12 16:20:45 +0530

    Improved aesthetic and added touch and drag effects

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function sendMail() {
     let params = {
         phno: phoneInput
     }
-    
+
     emailjs.send("service_2wu075r", "template_t1wfur8", params)
         .then(function(response) {
-            alert("Tis was my pleasure");
+            alert("Tis was my pleasure");   
             document.getElementById("ph-no").value = "";
         }, function(error) {
             alert("Alas! Something went awry... 😓\nThy message could not be sent. Try again anon.");
@@ -64,4 +64,67 @@ confirmYes.addEventListener("click", () => {
 confirmNo.addEventListener("click", () => {
     customConfirm.classList.add("hidden");
     alert("Thou art most gracious! My heart rejoiceth!");
+});
+
+let isDrawing = false;
+let lastDraw = 0;
+const drawInterval = 80;
+
+function createPetal(x, y) {
+  const petal = document.createElement('div');
+  petal.className = 'petal';
+  petal.style.left = `${x}px`;
+  petal.style.top = `${y}px`;
+
+  const moveX = (Math.random() - 0.5) * 100;  
+  const moveY = -60 - Math.random() * 60;
+  const rotation = (Math.random() - 0.5) * 180;
+
+  petal.style.setProperty('--x', `${moveX}px`);
+  petal.style.setProperty('--y', `${moveY}px`);
+  petal.style.setProperty('--rot', `${rotation}deg`);
+
+  document.body.appendChild(petal);
+
+  setTimeout(() => petal.remove(), 1600);
+}
+
+function handleDraw(x, y) {
+  const now = Date.now();
+  if (now - lastDraw > drawInterval) {
+    lastDraw = now;
+    createPetal(x, y);
+  }
+}
+
+document.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  handleDraw(e.clientX, e.clientY);
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (isDrawing) {
+    handleDraw(e.clientX, e.clientY);
+  }
+});
+
+document.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+document.addEventListener('touchstart', (e) => {
+  isDrawing = true;
+  const touch = e.touches[0];
+  handleDraw(touch.clientX, touch.clientY);
+});
+
+document.addEventListener('touchmove', (e) => {
+  if (isDrawing) {
+    const touch = e.touches[0];
+    handleDraw(touch.clientX, touch.clientY);
+  }
+});
+
+document.addEventListener('touchend', () => {
+  isDrawing = false;
 });
